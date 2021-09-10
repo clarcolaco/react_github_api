29fb64c7cfeadffdd380e8dc693fb19d21c77942
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-09-09 23:07:22 -0300

    Form/botton submit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import GithubImage from './github.png';
 import './App.css';
 
 function App() {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit!");
+
+  }
+
+
   return (
     <div className="container text-center">
       <h1 className="py-5">
-        Github profile
+        GITHUB PROFILE
       </h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
-         
           <div className="input-group">
 
-            <input 
-            type="text" 
-            className="form-control" 
-            required
+            <input
+              type="text"
+              className="form-control"
+              required
             />
             <span className="input-group-btn">
-              <button type="submit" className="btn btn-sucess">
+              <button type="submit" className="btn btn-outline-dark">
                 Search
               </button>
             </span>
@@ -26,6 +33,23 @@ function App() {
 
         </div>
       </form>
+      <div className="py-5">
+        <img
+          src={GithubImage}
+          className="responsive rounded circle"
+          alt=""
+          height="200px" />
+
+        <h1 className="pt-5">
+          <a href="https://github.com/clarcolaco/" target="_blank">Clarissa Colaco</a>
+        </h1>
+        <h3>Cascavel</h3>
+        <p>
+          <a href="https://www.linkedin.com/in/clarissa-colaco-ramos/" target="_blank" className="text-info">
+            Linkedin
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
