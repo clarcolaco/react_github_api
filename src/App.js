4a5d929b49cfba70f66750c7f3c841c4e1aa3c93
Clarissa Colaco <clarissa.colaco@hotmail.com>
2021-09-09 22:34:33 -0300

    Form

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ function App() {
   return (
     <div className="container text-center">
       <h1 className="py-5">
-      Github profile
+        Github profile
       </h1>
-    
+      <form>
+        <div className="form-group">
+         
+          <div className="input-group">
+
+            <input 
+            type="text" 
+            className="form-control" 
+            required
+            />
+            <span className="input-group-btn">
+              <button type="submit" className="btn btn-sucess">
+                Search
+              </button>
+            </span>
+          </div>
+
+        </div>
+      </form>
     </div>
   );
 }
